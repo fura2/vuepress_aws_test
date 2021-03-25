82fe0fb14902d92052f7a31b570b4b755b88d9d6
fura2 <yamazoe0577@gmail.com>
2021-03-25 16:37:52 +0900

    add config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	title: 'title',
+	locales: {
+		'/': {
+			lang: 'ja'
+		}
+	},
+	meta: [
+		{ charset: 'utf-8' },
+		{ name: 'viewport', content: 'width=device-width, initial-scale=1' }
+	],
+	themeConfig: {
+		sidebar: [
+			['/', 'Home'],
+			['/sub/', 'sub']
+		]
+	}
+}
